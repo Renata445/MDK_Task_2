d9fc647cbddca088f23f99fc05000439a65f644d
Renata445 <kisa09-10@mail.ru>
2023-01-26 13:30:34 +0300

    Task_7

---
+++ Task_7/Task_7.py
@@ -0,0 +1,8 @@
+x1 = int(input("Введите координату x: "))
+y1 = int(input("Введите координату y: "))
+x2 = int(input("Введите координату x: "))
+y2 = int(input("Введите координату y: "))
+if x1 == x2 or y1 == y2:
+    print("YES")
+else:
+    print("NO")
