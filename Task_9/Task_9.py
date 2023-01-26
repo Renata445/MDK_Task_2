b25edf8cdbbbb884370d86b07e7814d2b15492f3
Renata445 <kisa09-10@mail.ru>
2023-01-26 14:16:16 +0300

    Task_9

---
+++ Task_9/Task_9.py
@@ -0,0 +1,8 @@
+x1 = int(input("Введите координату x1: "))
+y1 = int(input("Введите координату y1: "))
+x2 = int(input("Введите координату x2: "))
+y2 = int(input("Введите координату y2: "))
+if abs(x1 - x2) == abs(y1 - y2):
+    print("YES")
+else:
+    print("NO")
